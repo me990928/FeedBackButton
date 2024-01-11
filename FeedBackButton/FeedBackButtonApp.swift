fefdde0919ed3ee2303c1ffef183cdeaeeb93bd8
me990928 <me990928@gmail.com>
2024-01-11 16:27:44 +0900

    Initial Commit

---
+++ FeedBackButton/FeedBackButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedBackButtonApp.swift
+//  FeedBackButton
+//
+//  Created by 広瀬友哉 on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct FeedBackButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
